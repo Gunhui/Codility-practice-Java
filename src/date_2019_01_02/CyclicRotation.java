8e8ac047372665dba7091582a69acc72489319b1
Gunhui <ygh1846@naver.com>
2019-01-02 22:50:43 +0900

    2019-01-02 practiced example named CyclicRotation

---
+++ src/date_2019_01_02/CyclicRotation.java
@@ -0,0 +1,18 @@
+package date_2019_01_02;
+
+public class CyclicRotation {
+	public int[] solution(int[] A, int K) {
+        int length = A.length;
+        int[] temp = new int[length];
+        for(int i=0; i<K; i++){
+            temp[0] = A[length-1];
+            for(int j=0; j<length-1; j++){
+               temp[j+1] = A[j];    
+            }
+            for(int k=0; k<length; k++){
+                A[k] = temp[k];
+            }
+        }
+        return temp;
+    }
+}
